cd1d6683a3a9cd0c5997498425a71e965f89b775
PAUL <you@example.com>
2022-11-06 16:24:07 +0300

    adding alcohol db

--- itisha/itishaDrinks/models.py
+++ itisha/itishaDrinks/models.py
@@ -4,6 +4,33 @@ from django.db import models
 # Create your models here.\
 
 
+ALCOHOL_CATEGORY = (
+    ('', 'Gin'),
+    ('W', 'Whiskey'),
+    ('V', 'Vodka'),
+    
+)
+
+class alcohol_drinks(models.Model):
+  drink_slug = models.CharField(max_length=250, primary_key=True, null=False, help_text = "the names of the product SHOULD be unique to every product.")  #this is a slugify design to map web pages amd identify products
+  drink_type = models.CharField(max_length=250, null=False, blank=False, help_text = "this is the drink category, either alcohol, Energy drink, juice ...")
+  drink_name = models.CharField(max_length=250, null=False, blank=False, help_text = "Can be same to drink slug, (but not every time)")  
+  drink_qty = models.CharField(max_length=250, null=False, blank=False)
+  drink_price = models.IntegerField()
+  alcohol_drinks_type = models.CharField(choices=ALCOHOL_CATEGORY, max_length=20)
+  drink_description = models.CharField(max_length=250, null=False, blank=False)
+  top_rated =models.BooleanField(default=False, help_text = "categorised under the top rated category")
+  recommended =models.BooleanField(default=False, help_text = " recommencded to other users")
+  offers =models.BooleanField(default=False, help_text = " categorised as 15% off")
+  
+  
+
+  def __str__(self):
+    return self.drink_name
+
+
+
+
 
 class drink_shop(models.Model):
   shop_id = models.IntegerField()
